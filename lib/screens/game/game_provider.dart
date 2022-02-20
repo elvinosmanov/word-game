248e8134c7e8a265b28a0cd3004b0b6587a7387a
elvinosmanov <youngtronik@gmail.com>
2022-02-20 11:01:39 +0400

    done

--- lib/screens/game/game_provider.dart
+++ lib/screens/game/game_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -33,8 +35,17 @@ class GameProvider with ChangeNotifier {
   }
 
   List<int?> previousOn = [];
-  double choosenBoxSize = 70;
+  double _choosenBoxSize = 70;
+
+  double get choosenBoxSize => _choosenBoxSize;
+
+  set choosenBoxSize(double choosenBoxSize) {
+    _choosenBoxSize = choosenBoxSize;
+    notifyListeners();
+  }
+
   List<String> randomCharacters = [];
+
   List<Widget> rowChildren = <Widget>[];
 
   void reCreateBoxes() {
@@ -80,8 +91,26 @@ class GameProvider with ChangeNotifier {
         counter += 1;
       }
     }
-
     choosenBoxSize = 70;
+  }
+
+  onPanUpdate(DragUpdateDetails details) {
+    choosenBoxSize = 60;
+    offset = Offset(offset.dx + details.delta.dx, offset.dy + details.delta.dy);
+    if (_findBox(offset)[0] != -1) {
+      if (previousOn.isNotEmpty) {
+        boxModels[previousOn[0]!][previousOn[1]!].isOn = false;
+      }
+      final model = boxModels[_findBox(offset)[0]][_findBox(offset)[1]];
+      previousOn = [_findBox(offset)[0], _findBox(offset)[1]];
+      if (!model.isOn) {
+        model.isOn = true;
+      }
+    } else {
+      if (previousOn.isNotEmpty) {
+        boxModels[previousOn[0]!][previousOn[1]!].isOn = false;
+      }
+    }
   }
 
   List _findBox(Offset position) {
@@ -104,9 +133,15 @@ class GameProvider with ChangeNotifier {
   }
 
   Size _positionBox(int col, int row) {
-    final dx =
-        width / 2 - kBoxSize / 2 - (kBoxSize + kDistanceBetweenBox) * 2;
+    final dx = width / 2 - kBoxSize / 2 - (kBoxSize + kDistanceBetweenBox) * 2;
     final dy = height / 2 - (kBoxSize - 2) * 3;
     return Size(dx + row * (kBoxSize + kDistanceBetweenBox), dy + col * (kBoxSize - 2));
+  }
+
+  String getRandom({int? length}) {
+    const ch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ???';
+    Random r = Random();
+    return String.fromCharCodes(
+        Iterable.generate(length ?? 1, (_) => ch.codeUnitAt(r.nextInt(ch.length))));
   }
 }
--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -1,31 +1,16 @@
-import 'dart:math';
-
-import 'package:demo_bloc/core/constants/game_constants.dart';
 import 'package:demo_bloc/screens/game/game_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
-
   @override
   _GameScreenState createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
-  List<int?> previousOn = [];
-  double choosenBoxSize = 70;
   late GameProvider provider;
   bool firstTime = true;
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,14 +19,10 @@ class _GameScreenState extends State<GameScreen> {
     provider.width = MediaQuery.of(context).size.width;
     provider.reCreateBoxes();
     if (firstTime) {
-      provider.randomCharacters = getRandom(length: 28).split('');
-      Future.delayed(
-        const Duration(microseconds: 1),
-        () {
-          provider.offset = Offset(provider.width / 2 - 35, provider.height / 2 + 187);
-        },
-      );
-
+      provider.randomCharacters = provider.getRandom(length: 28).split('');
+      Future.delayed(const Duration(microseconds: 1), () {
+        provider.offset = Offset(provider.width / 2 - 35, provider.height / 2 + 187);
+      });
       firstTime = false;
     }
     return Scaffold(
@@ -60,35 +41,18 @@ class _GameScreenState extends State<GameScreen> {
             top: provider.offset.dy,
             child: GestureDetector(
               onPanStart: ((details) =>
-                  provider.offset = Offset(provider.offset.dx, provider.offset.dy)),
+                  provider.offset = Offset(provider.offset.dx, provider.offset.dy - 30)),
               onPanEnd: (details) {
                 // isDragged = false;
                 provider.onPanEnd();
                 provider.offset = Offset(provider.width / 2 - 35, provider.height / 2 + 187);
               },
               onPanUpdate: (details) {
-                choosenBoxSize = 60;
-                provider.offset = Offset(
-                    provider.offset.dx + details.delta.dx, provider.offset.dy + details.delta.dy);
-                if (_findBox(provider.offset)[0] != -1) {
-                  if (previousOn.isNotEmpty) {
-                    provider.boxModels[previousOn[0]!][previousOn[1]!].isOn = false;
-                  }
-                  final model = provider.boxModels[_findBox(provider.offset)[0]]
-                      [_findBox(provider.offset)[1]];
-                  previousOn = [_findBox(provider.offset)[0], _findBox(provider.offset)[1]];
-                  if (!model.isOn) {
-                    model.isOn = true;
-                  }
-                } else {
-                  if (previousOn.isNotEmpty) {
-                    provider.boxModels[previousOn[0]!][previousOn[1]!].isOn = false;
-                  }
-                }
+                provider.onPanUpdate(details);
               },
               child: Container(
-                width: choosenBoxSize,
-                height: choosenBoxSize,
+                width: provider.choosenBoxSize,
+                height: provider.choosenBoxSize,
                 decoration:
                     BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(5)),
                 child: Center(
@@ -99,50 +63,8 @@ class _GameScreenState extends State<GameScreen> {
               ),
             ),
           ),
-          // Positioned(
-          //   top: MediaQuery.of(context).size.height / 2 + 187,
-          //   left: MediaQuery.of(context).size.width / 2 - 35,
-          //   child: Container(
-          //     width: 70,
-          //     height: 70,
-          //     decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(5)),
-          //   ),
-          // ),
         ],
       ),
     );
-  }
-
-  Size _positionBox(int col, int row) {
-    final dx =
-        MediaQuery.of(context).size.width / 2 - kBoxSize / 2 - (kBoxSize + kDistanceBetweenBox) * 2;
-    final dy = MediaQuery.of(context).size.height / 2 - (kBoxSize - 2) * 3;
-    return Size(dx + row * (kBoxSize + kDistanceBetweenBox), dy + col * (kBoxSize - 2));
-  }
-
-  bool _isOnBox(int col, int row) {
-    const double padding = kBoxSize / 2 + 4;
-    return provider.offset.dx >= _positionBox(col, row).width - padding &&
-        provider.offset.dx <= _positionBox(col, row).width + padding &&
-        provider.offset.dy >= _positionBox(col, row).height - padding &&
-        provider.offset.dy <= _positionBox(col, row).height + padding;
-  }
-
-  List _findBox(Offset position) {
-    for (var i = 0; i < kRowNumber; i++) {
-      for (var j = 0; j < kColNumber; j++) {
-        if (_isOnBox(i, j)) {
-          return [i, j];
-        }
-      }
-    }
-    return [-1, -1];
-  }
-
-  String getRandom({int? length}) {
-    const ch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ???';
-    Random r = Random();
-    return String.fromCharCodes(
-        Iterable.generate(length ?? 1, (_) => ch.codeUnitAt(r.nextInt(ch.length))));
   }
 }
